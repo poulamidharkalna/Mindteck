200f74edd55c2ee2f435c42a9de9e5fdc9ca0fb6
poulamidharkalna <99160237+poulamidharkalna@users.noreply.github.com>
2022-08-17 17:37:13 +0530

    Add files via upload

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Delegate
+{
+    class poulami
+    {
+        public delegate void poulamidelegate(string name);
+        public delegate void Multidelegate(int x, int y);
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine(x * y);
+
+        }
+        public void name(string name)
+        {
+            Console.WriteLine("my name is" + " " + name);
+
+        }
+        static void Main(string[] args)
+        {
+            poulami d = new poulami();
+            poulamidelegate obj1 = new poulamidelegate(d.name);
+            Multidelegate obj2 = new Multidelegate(d.Multi);
+            obj1("Poulami");
+            obj2(20, 20);
+
+        }
+
+    }
+
+}
