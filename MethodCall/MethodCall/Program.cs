6254a18f0c696be54c3f08125e721fab83997012
poulamidharkalna <99160237+poulamidharkalna@users.noreply.github.com>
2022-08-18 16:00:45 +0530

    Add files via upload

---
+++ MethodCall/MethodCall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MethodCall
+{
+    class vechile
+    {
+        string Brand_Name;
+        public void Quality()
+        {
+            Console.WriteLine("Quality is so good");
+        }
+        public void Price(int x)
+        {
+            Console.WriteLine("The price of the Vechile is" + "" + "" + x);
+        }
+        public void Customer_Support(string name)
+        {
+            Console.WriteLine("Customer support is " + "" + name);
+        }
+        static void Main()
+        {
+            vechile v = new vechile();
+            v.Brand_Name = "Royal Enfield";
+            v.Quality();
+            v.Price(120000);
+            v.Customer_Support("Good");
+            Console.WriteLine(v.Brand_Name = "Royal Enfield");
+            Console.ReadLine();
+
+
+        }
+    }
+}
